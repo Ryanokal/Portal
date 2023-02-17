d2a164f3fbf1df47bf707bcb1309023699d0a961
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2023-02-17 22:04:36 +0300

    KNN - f1 score, recall score #1

--- model.py
+++ model.py
@@ -62,6 +62,7 @@ print('Prediction: {}'.format(prediction))
 
 # train test split
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, recall_score
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
 knn = KNeighborsClassifier(n_neighbors=3)
@@ -69,12 +70,17 @@ x, y = data.loc[:, data.columns != 'Target'], data.loc[:, 'Target']
 knn.fit(x_train, y_train)
 prediction = knn.predict(x_test)
 # print('Prediction: {}'.format(prediction))
+print("KNN\n")
 print('With KNN (K=3) accuracy is: ', knn.score(x_test, y_test))  # ACCURACY = 0.83
+print(f"F1 Score: {f1_score(y_test, knn.predict(x_test), average='macro'):.2f}")
+print(f"Recall: {recall_score(y_test, knn.predict(x_test), average='macro'):.2f}\n")
 
 # Model complexity
 neig = np.arange(1, 25)
 train_accuracy = []
 test_accuracy = []
+test_recall = []
+test_f1 = []
 
 # Loop over different values of k
 for i, k in enumerate(neig):
@@ -86,19 +92,32 @@ for i, k in enumerate(neig):
     train_accuracy.append(knn.score(x_train, y_train))
     # test accuracy
     test_accuracy.append(knn.score(x_test, y_test))
+    # train recall
+    test_recall.append(recall_score(y_train, knn.predict(x_train)))
+    # test recall
+    test_recall.append(recall_score(y_test, knn.predict(x_test)))
+    # train f1 score
+    test_f1.append(f1_score(y_train, knn.predict(x_train)))
+    # test f1 score
+    test_f1.append(f1_score(y_test, knn.predict(x_test)))
+
 
 # Plot
 plt.figure(figsize=[13, 8])
 plt.plot(neig, test_accuracy, label='Testing Accuracy')
 plt.plot(neig, train_accuracy, label='Training Accuracy')
+plt.plot(neig, test_f1, label='Testing F1 Score')
+plt.plot(neig, train_accuracy, label='Training Accuracy')
 plt.legend()
-plt.title('-value VS Accuracy')
+plt.title('-value VS Accuracy, Recall, and F1 Score')
 plt.xlabel('Number of Neighbors')
-plt.ylabel('Accuracy')
+plt.ylabel('Metric Score')
 plt.xticks(neig)
 plt.savefig('graph.png')
 plt.show()
 print("Best accuracy is {} with K = {}".format(np.max(test_accuracy), 1 + test_accuracy.index(np.max(test_accuracy))))
+print("Best f1 score is {} with K = {}".format(np.max(test_f1)), 1 + test_accuracy.index(np.max(test_accuracy)))
+print("Best recall is {} with K = {}\n".format(np.max(test_recall), 1 + test_accuracy.index(np.max(test_accuracy))))
 
 # Logistic regression - accuracy = 91.83%
 from sklearn.linear_model import LogisticRegression
@@ -108,9 +127,10 @@ lr_clf = LogisticRegression(solver='liblinear')
 lr_clf.fit(x_train, y_train)
 
 pred = lr_clf.predict(x_test)
+print("LOGISTIC REGRESSION")
 print(f"Accuracy Score: {accuracy_score(y_test, pred) * 100:.2f}%")
 print(f"F1 Score: {f1_score(y_test, pred, average='macro'):.2f}")
-print(f"Recall: {recall_score(y_test, pred, average='macro'):.2f}")
+print(f"Recall: {recall_score(y_test, pred, average='macro'):.2f}\n")
 
 # Support Vector Machine (SVM)- accuracy  = 60.79%
 from sklearn.svm import SVC
@@ -119,7 +139,10 @@ svm_clf = SVC(kernel='rbf', gamma=0.1, C=1.0)
 svm_clf.fit(x_train, y_train)
 
 pred = svm_clf.predict(x_test)
+print("SUPPORT VECTOR MACHINE")
 print(f"Accuracy Score: {accuracy_score(y_test, pred) * 100:.2f}%")
+print(f"F1 Score: {f1_score(y_test, pred, average='macro'):.2f}")
+print(f"Recall: {recall_score(y_test, pred, average='macro'):.2f}\n")
 
 # Decision Tree - accuracy  = 86.96%
 from sklearn.tree import DecisionTreeClassifier
@@ -128,7 +151,10 @@ tree_clf = DecisionTreeClassifier(random_state=40)
 tree_clf.fit(x_train, y_train)
 
 pred = tree_clf.predict(x_test)
+print("DECISION TREE")
 print(f"Accuracy Score: {accuracy_score(y_test, pred) * 100:.2f}%")
+print(f"F1 Score: {f1_score(y_test, pred, average='macro'):.2f}")
+print(f"Recall: {recall_score(y_test, pred, average='macro'):.2f}")
 
 # save the model to disk
 joblib.dump(tree_clf, "student_model.sav")
